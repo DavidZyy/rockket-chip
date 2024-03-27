d663cd47afa04278e969f64a6b473709a9e55cd2
DavidZyy <1929772352@qq.com>
2024-03-27 20:46:40 +0800

    add out_class

--- src/main/scala/ysyxSoC/SoC.scala
+++ src/main/scala/ysyxSoC/SoC.scala
@@ -31,20 +31,22 @@ class ysyxSoCASIC(hasChipLink: Boolean)(implicit p: Parameters) extends LazyModu
   val chiplinkNode = if (hasChipLink) Some(AXI4SlaveNodeGenerator(p(ExtBus), ChipLinkParam.allSpace)) else None
 
   val luart = LazyModule(new APBUart16550(AddressSet.misaligned(0x10000000, 0x1000)))
-  val lgpio = LazyModule(new APBGPIO(AddressSet.misaligned(0x10002000, 0x10)))
-  val lkeyboard = LazyModule(new APBKeyboard(AddressSet.misaligned(0x10011000, 0x8)))
-  val lvga = LazyModule(new APBVGA(AddressSet.misaligned(0x21000000, 0x200000)))
-  val lspi  = LazyModule(new APBSPI(
-    AddressSet.misaligned(0x10001000, 0x1000) ++    // SPI controller
-    AddressSet.misaligned(0x30000000, 0x10000000)   // XIP flash
-  ))
-  val lpsram = LazyModule(new APBPSRAM(AddressSet.misaligned(0x80000000L, 0x400000)))
-  val lmrom = LazyModule(new AXI4MROM(AddressSet.misaligned(0x20000000, 0x1000)))
-  val sramNode = AXI4RAM(AddressSet.misaligned(0x0f000000, 0x2000).head, false, true, 8, None, Nil, false)
-  val lsdram = LazyModule(new APBSDRAM(AddressSet.misaligned(0xa0000000L, 0x2000000)))
+  // val lgpio = LazyModule(new APBGPIO(AddressSet.misaligned(0x10002000, 0x10)))
+  // val lkeyboard = LazyModule(new APBKeyboard(AddressSet.misaligned(0x10011000, 0x8)))
+  // val lvga = LazyModule(new APBVGA(AddressSet.misaligned(0x21000000, 0x200000)))
+  // val lspi  = LazyModule(new APBSPI(
+  //   AddressSet.misaligned(0x10001000, 0x1000) ++    // SPI controller
+  //   AddressSet.misaligned(0x30000000, 0x10000000)   // XIP flash
+  // ))
+  // val lpsram = LazyModule(new APBPSRAM(AddressSet.misaligned(0x80000000L, 0x400000)))
+  // val lmrom = LazyModule(new AXI4MROM(AddressSet.misaligned(0x20000000, 0x1000)))
+  // val sramNode = AXI4RAM(AddressSet.misaligned(0x0f000000, 0x2000).head, false, true, 8, None, Nil, false)
+  // val lsdram = LazyModule(new APBSDRAM(AddressSet.misaligned(0xa0000000L, 0x2000000)))
 
-  List(lspi.node, luart.node, lpsram.node, lsdram.node, lgpio.node, lkeyboard.node, lvga.node).map(_ := apbxbar)
-  List(apbxbar := AXI4ToAPB(), lmrom.node, sramNode).map(_ := xbar)
+  // List(lspi.node, luart.node, lpsram.node, lsdram.node, lgpio.node, lkeyboard.node, lvga.node).map(_ := apbxbar)
+  // List(apbxbar := AXI4ToAPB(), lmrom.node, sramNode).map(_ := xbar)
+  List(luart.node).map(_ := apbxbar)
+  List(apbxbar := AXI4ToAPB()).map(_ := xbar)
   if (hasChipLink) chiplinkNode.get := xbar
   xbar := cpu.masterNode
 
@@ -73,21 +75,24 @@ class ysyxSoCASIC(hasChipLink: Boolean)(implicit p: Parameters) extends LazyModu
     val intr_from_chipSlave = IO(Input(Bool()))
     cpu.module.interrupt := intr_from_chipSlave
 
+    val out = IO(new out_class)
+    out <> cpu.module.out
+
     // expose slave I/O interface as ports
-    val spi = IO(chiselTypeOf(lspi.module.spi_bundle))
+    // val spi = IO(chiselTypeOf(lspi.module.spi_bundle))
     val uart = IO(chiselTypeOf(luart.module.uart))
-    val psram = IO(chiselTypeOf(lpsram.module.qspi_bundle))
-    val sdram = IO(chiselTypeOf(lsdram.module.sdram_bundle))
-    val gpio = IO(chiselTypeOf(lgpio.module.gpio_bundle))
-    val ps2 = IO(chiselTypeOf(lkeyboard.module.ps2_bundle))
-    val vga = IO(chiselTypeOf(lvga.module.vga_bundle))
+    // val psram = IO(chiselTypeOf(lpsram.module.qspi_bundle))
+    // val sdram = IO(chiselTypeOf(lsdram.module.sdram_bundle))
+    // val gpio = IO(chiselTypeOf(lgpio.module.gpio_bundle))
+    // val ps2 = IO(chiselTypeOf(lkeyboard.module.ps2_bundle))
+    // val vga = IO(chiselTypeOf(lvga.module.vga_bundle))
     uart <> luart.module.uart
-    spi <> lspi.module.spi_bundle
-    psram <> lpsram.module.qspi_bundle
-    sdram <> lsdram.module.sdram_bundle
-    gpio <> lgpio.module.gpio_bundle
-    ps2 <> lkeyboard.module.ps2_bundle
-    vga <> lvga.module.vga_bundle
+    // spi <> lspi.module.spi_bundle
+    // psram <> lpsram.module.qspi_bundle
+    // sdram <> lsdram.module.sdram_bundle
+    // gpio <> lgpio.module.gpio_bundle
+    // ps2 <> lkeyboard.module.ps2_bundle
+    // vga <> lvga.module.vga_bundle
   }
 }
 
@@ -125,28 +130,31 @@ class ysyxSoCFull(implicit p: Parameters) extends LazyModule {
 
     masic.intr_from_chipSlave := false.B
 
-    val flash = Module(new flash)
-    flash.io <> masic.spi
-    flash.io.ss := masic.spi.ss(0)
-    val bitrev = Module(new bitrev)
-    bitrev.io <> masic.spi
-    bitrev.io.ss := masic.spi.ss(7)
-    masic.spi.miso := List(bitrev.io, flash.io).map(_.miso).reduce(_&&_)
+    // val flash = Module(new flash)
+    // flash.io <> masic.spi
+    // flash.io.ss := masic.spi.ss(0)
+    // val bitrev = Module(new bitrev)
+    // bitrev.io <> masic.spi
+    // bitrev.io.ss := masic.spi.ss(7)
+    // masic.spi.miso := List(bitrev.io, flash.io).map(_.miso).reduce(_&&_)
 
-    val psram = Module(new psram)
-    psram.io <> masic.psram
-    val sdram = Module(new sdram)
-    sdram.io <> masic.sdram
+    // val psram = Module(new psram)
+    // psram.io <> masic.psram
+    // val sdram = Module(new sdram)
+    // sdram.io <> masic.sdram
 
     val externalPins = IO(new Bundle{
-      val gpio = chiselTypeOf(masic.gpio)
-      val ps2 = chiselTypeOf(masic.ps2)
-      val vga = chiselTypeOf(masic.vga)
+      // val gpio = chiselTypeOf(masic.gpio)
+      // val ps2 = chiselTypeOf(masic.ps2)
+      // val vga = chiselTypeOf(masic.vga)
       val uart = chiselTypeOf(masic.uart)
     })
-    externalPins.gpio <> masic.gpio
-    externalPins.ps2 <> masic.ps2
-    externalPins.vga <> masic.vga
+    // externalPins.gpio <> masic.gpio
+    // externalPins.ps2 <> masic.ps2
+    // externalPins.vga <> masic.vga
     externalPins.uart <> masic.uart
+
+    val out = IO(new out_class)
+    out <> masic.out
   }
 }
--- src/main/scala/ysyxSoC/TestHarness.scala
+++ src/main/scala/ysyxSoC/TestHarness.scala
@@ -6,9 +6,13 @@ import freechips.rocketchip.diplomacy.LazyModule
 import ysyx._
 
 class TestHarness()(implicit p: Parameters) extends Module {
-  val io = IO(new Bundle { })
+  val io = IO(new Bundle { 
+    val out = (new out_class)
+  })
   val ldut = LazyModule(new ysyxSoCFull)
   val dut = Module(ldut.module)
   dut.dontTouchPorts()
   dut.externalPins := DontCare
+
+  io.out <> dut.out
 }
